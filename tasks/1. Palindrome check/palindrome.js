cc6ea08aa5d67473fc2f237d1c24dcd2b7042398
mtmazzo <74214745+mtmazzo@users.noreply.github.com>
2021-08-24 01:52:56 -0400

    Update palindrome.js

--- tasks/1. Palindrome check/palindrome.js
+++ tasks/1. Palindrome check/palindrome.js
@@ -1,4 +1,6 @@
 function isPalindrome(inputString) {
+export function isPalindrome(inputString) {
   var revString = inputString.split('').reverse().join('');
   return (inputString == revString) ? "true" : "false";
+  return (inputString == revString) ? true : false;
 }
