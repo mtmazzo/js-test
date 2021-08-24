689416676f0091105639a65b571a9ddf0e351265
mtmazzo <74214745+mtmazzo@users.noreply.github.com>
2021-08-24 19:19:27 -0400

    Update balancedParentheses.js

--- tasks/3. Balanced parentheses/balancedParentheses.js
+++ tasks/3. Balanced parentheses/balancedParentheses.js
@@ -1,3 +1,9 @@
 export function areParenthesesBalanced(inputString) {
-  // TODO: write your code here
+  var o = 0;
+  var c = 0;
+  for(var i = 0; i < inputString.length; i++) {
+    if(inputString.charAt(i) == "(") o++;
+    if(inputString.charAt(i) == ")") c++;
+  }
+  return (o == c) ? true : false;
 }
