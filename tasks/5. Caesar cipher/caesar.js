8cdab49b93e20fb961e14ec2fe8c5627cb8b6f62
mtmazzo <74214745+mtmazzo@users.noreply.github.com>
2021-08-24 20:33:13 -0400

    Update caesar.js

--- tasks/5. Caesar cipher/caesar.js
+++ tasks/5. Caesar cipher/caesar.js
@@ -1,3 +1,13 @@
 export function encryptCaesar(inputString, key) {
-  // TODO: write your code here
+  var result = "";
+  for(var i = 0; i < inputString.length; i++) {
+    var char = inputString[i];
+    if(char.match(/[a-z]/gi)) {
+      var code = inputString.charCodeAt(i);
+      if(code >= 65 && code <= 90) char = String.fromCharCode(((code - 65 + key) % 26) + 65);
+      else if(code >= 97 && code <= 122) char = String.fromCharCode(((code - 97 + key) % 26) + 97);
+    }
+    result += char;
+  }
+  return result;
 }
